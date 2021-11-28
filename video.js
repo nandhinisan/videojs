8957b31f72ce5120f688097048d4a9b5d2c22194
nandhinisan <95136768+nandhinisan@users.noreply.github.com>
2021-11-28 20:24:35 +0530

    videojs webpage

---
+++ video.js
@@ -0,0 +1,15 @@
+var myvid = document.getElementById('fm-video');
+var myvids = [
+  "pexels-kelly-lacy-9722139.mp4",
+  "production ID_4762904.mp4"
+  ];
+var activeVideo = 0;
+
+myvid.addEventListener('ended', function(e) {
+  // update the active video index
+  activeVideo = (++activeVideo) % myvids.length;
+
+  // update the video source and play
+  myvid.src = myvids[activeVideo];
+  myvid.play();
+});
